64038561c0251f4d7c402ea60a9551787b943caf
Chris Gilbert <chrisgilbert4@icloud.com>
2020-12-14 13:03:58 +0000

    Add a rough nav

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,20 @@
+import styles from "./Header.module.css";
+
 const Header = () => {
-  return <nav></nav>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <span>Gluten Free Advisor</span>
+        <nav className={styles.nav}>
+          <ul>
+            <li>Discover</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
